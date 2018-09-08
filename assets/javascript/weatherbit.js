bac27832f86295de0c78239d9fd2eaebe898f4a7
irvingrivas <irving0594@gmail.com>
2018-09-08 13:32:31 -0700

    Finished adding variables

--- assets/javascript/weatherbit.js
+++ assets/javascript/weatherbit.js
@@ -5,8 +5,10 @@ var start_date = moment($("#start-date").val());
 var end_date = moment($("#end-date").val());
 
 // Only get up to 16 days forecast, otherwise goes back a year
+var prediction = "forecast";
 var curr_date = moment();
 if (curr_date.diff(end_date,"days") >= 16) { 
+    prediction = "history";
     start_date = start_date.subtract(1,"years");
     end_date = end_date.subtract(1,"years");
 }
@@ -29,15 +31,16 @@ if (country === "US") {
 // This is our weather bit API key
 var APIKey = "5887b8d504574dffbe86fb6dfad4bd60";
 
+// DO NOT Run this repeatedly, API only allows a finite number of calls for weather data !!
 for (let i = 0; i < date_arr.length; ++i) {
 
     // Here we are building the URL we need to query the database   
     if (is_US_state) {
-        var queryURL = "https://api.weatherbit.io/v2.0/history/daily?" +
+        var queryURL = "https://api.weatherbit.io/v2.0/" + prediction + "/daily?" +
         "city=" + city + "," + state + "," + country + "&start_date=" +  start_date +
         "&end_date" + end_date + "&key=" + APIKey;
     } else {
-        var queryURL = "https://api.weatherbit.io/v2.0/history/daily?" +
+        var queryURL = "https://api.weatherbit.io/v2.0/" + prediction + "/daily?" +
         "city=" + city + "," + country + "&start_date=" +  start_date +
         "&end_date" + end_date + "&key=" + APIKey;  
     }
@@ -48,11 +51,20 @@ for (let i = 0; i < date_arr.length; ++i) {
             method: "GET"
         })
             // We store all of the retrieved data inside of an object called "response"
+            // Documentation available at https://www.weatherbit.io/api/weather-forecast-16-day
             .then(function(response) {
-                $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-                $(".wind").text("Wind Speed: " + response.wind.speed);
-                $(".humidity").text("Humidity: " + response.main.humidity);
-                $(".temp").text("Temperature (F) " + response.main.temp);
+                $("#city-div").append("<p>City: " + reponse.city-name + "</p>");
+                $("#temp-div").append("<p>Temperature (F): " + response.data.temp + "</p>");
+                $("#max-temp-div").append("<p> Max Temperature (F): " + response.data.max_temp + "</p>");
+                $("#min-temp-div").append("<p> Min Temperature (F): " + response.data.min_temp + "</p>");
+                $("#wind-speed-div").append("<p> Windspeed (mph): " + response.data.wind_spd + "</p>"); 
+                $("#wind-dir-div").append("<p> Wind direction: " + response.data.wind_cdir_full + "</p>"); 
+                $("#clouds-div").append("<p> Clouds: " + response.data.clouds + "</p>");
+                $("#visibility-div").append("<p> Visibility %: " + response.data.vis + "</p>");
+                $("#snow-div").append("<p> Snow: " + response.data.snow + "</p>");
+                $("#snow-depth-div").append("<p> Snow Depth: " + response.data.snow_depth + "</p>");
+                
+                 console.log(JSON.stringify(response));
             });
 }
     
