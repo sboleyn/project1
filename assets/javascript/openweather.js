03e3329cdbcddab0f9ea1f1b890b99bab5b3ec65
irvingrivas <irving0594@gmail.com>
2018-09-08 12:22:30 -0700

    Using different weather API

--- assets/javascript/openweather.js
+++ assets/javascript/openweather.js
@@ -1,14 +1,28 @@
- // This is our open weather API key
-var APIKey = "166a433c57516f51dfab1f7edaed8413";
+// This is our weather bit API key
+var APIKey = "5887b8d504574dffbe86fb6dfad4bd60";
 
+// Necessary variables for weather
 var city = $("#city").val();
 var country = $("#country").val();
-var today_unix = Date.now(); // unix time
-var today_moment = moment(); // moment time
+var start_date = $("#start-date").val();
+var end_date = $("#end-date").val();
+if (country === "US") {
+    var is_US_state = true;
+    var state = $("#state").val();
+} else {
+    var is_US_state = false;
+}
 
 // Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=" + city + "," + country + "&units=imperial&appid=" + APIKey;
+if (is_US_state) {
+    var queryURL = "https://api.weatherbit.io/v2.0/history/daily?" +
+    "city=" + city + "," + state + "," + country + "&start_date=" +  start_date +
+    "&end_date" + end_date + "&key=" + APIKey;
+} else {
+    var queryURL = "https://api.weatherbit.io/v2.0/history/daily?" +
+    "city=" + city + "," + country + "&start_date=" +  start_date +
+    "&end_date" + end_date + "&key=" + APIKey;  
+}
 
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
