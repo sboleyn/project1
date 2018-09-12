05bf5d4e48b8e5df942989a8670411b124b58c47
Sami B <sboleyn@email.arizona.edu>
2018-09-11 17:31:42 -0700

    fix to deleting my whole file

--- js/travelapi.js
+++ js/travelapi.js
@@ -1,20 +1,22 @@
 "use strict";
+
 /*
-music
-trails
-parks
-food
-coffee
-events */
+music, 103
+food and drink, 110
+Travel & Outdoor 109
+performing and visutal arts 105
+Sports & Fitness 108
+Seasonal & Holiday 116 */
+
 exports.__esModule = true;
-// Use this API call to get Eventbrite Category Ids https://www.eventbriteapi.com/v3/categories/?token=B7UELWTCRT7HCHZN3I7S
 var moment = require("moment");
-var dateFormat = "MM/DD/YYYY";
-// Necessary variables for weather
+// Use this API call to get Eventbrite Category Ids https://www.eventbriteapi.com/v3/categories/?token=B7UELWTCRT7HCHZN3I7S
+var dateFormat = "YYYY-MM-DDT00:00:00";
+// Necessary letiables for weather
 var city = $("#city").val();
 var country = $("#country").val();
-var start_date = moment($("#start-date").val());
-var end_date = moment($("#end-date").val());
+var start_date = moment($("#start-date").val()).format(dateFormat);
+var end_date = moment($("#end-date").val()).format(dateFormat);
 var queryURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=best&token=B7UELWTCRT7HCHZN3I7S";
 // Convert start and end dates in this format YYYY-MM-DDThh:mm:ssZ or YYYY-MM-DDThh:mm:ss
 // start_date.range_start=2019-01-01T00:00:00&start_date.range_end=2019-02-01T00:00:00
