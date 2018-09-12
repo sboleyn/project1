34464eb635a1f7366a0aeeeae998413afc9c9dc9
Sami B <sboleyn@email.arizona.edu>
2018-09-11 17:17:23 -0700

    ts is compiled to this file

---
+++ js/travelapi.js
@@ -0,0 +1,31 @@
+"use strict";
+/*
+music
+trails
+parks
+food
+coffee
+events */
+exports.__esModule = true;
+// Use this API call to get Eventbrite Category Ids https://www.eventbriteapi.com/v3/categories/?token=B7UELWTCRT7HCHZN3I7S
+var moment = require("moment");
+var dateFormat = "MM/DD/YYYY";
+// Necessary variables for weather
+var city = $("#city").val();
+var country = $("#country").val();
+var start_date = moment($("#start-date").val());
+var end_date = moment($("#end-date").val());
+var queryURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=best&token=B7UELWTCRT7HCHZN3I7S";
+// Convert start and end dates in this format YYYY-MM-DDThh:mm:ssZ or YYYY-MM-DDThh:mm:ss
+// start_date.range_start=2019-01-01T00:00:00&start_date.range_end=2019-02-01T00:00:00
+//Need to get categories based off which picture is clicked, each picture should have onclick function that populates a categories into the query.
+// Example return Music & Conversation with Marty
+// Tue, Oct 23, 6:00 PM
+// The cutting room, newyork ny
+// starts at 27.00 on ticket fly (link to ticket)
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    .then(function () {
+});
