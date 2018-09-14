5866f0f7c3ae333a28a39f9abb53f3fef89922e6
irvingrivas <irving0594@gmail.com>
2018-09-14 10:05:48 -0700

    Finshed weather app JS

--- js/travelapi.js
+++ js/travelapi.js
@@ -1,5 +1,4 @@
 "use strict";
-
 /*
 music, 103
 food and drink, 110
@@ -8,26 +7,48 @@ performing and visutal arts 105
 Sports & Fitness 108
 Seasonal & Holiday 116 */
 
-exports.__esModule = true;
-var moment = require("moment");
 // Use this API call to get Eventbrite Category Ids https://www.eventbriteapi.com/v3/categories/?token=B7UELWTCRT7HCHZN3I7S
+
+// Convert start and end dates in this format YYYY-MM-DDThh:mm:ssZ or YYYY-MM-DDThh:mm:ss
 var dateFormat = "YYYY-MM-DDT00:00:00";
-// Necessary letiables for weather
+// var dateFormat = "YYYY-MM-DDThh:mm:ss";
+
+// Necessary variable for weather
 var city = $("#city").val();
 var country = $("#country").val();
-var start_date = moment($("#start-date").val()).format(dateFormat);
-var end_date = moment($("#end-date").val()).format(dateFormat);
-var queryURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=best&token=B7UELWTCRT7HCHZN3I7S";
-// Convert start and end dates in this format YYYY-MM-DDThh:mm:ssZ or YYYY-MM-DDThh:mm:ss
-// start_date.range_start=2019-01-01T00:00:00&start_date.range_end=2019-02-01T00:00:00
+// let start_date = moment($("#start-date").val()).format(dateFormat);
+// let end_date = moment($("#end-date").val()).format(dateFormat);
+
+// for testing
+var start_date = moment().format(dateFormat);
+var end_date = moment().add(10, 'days').format(dateFormat);
+console.log(start_date);
+console.log(end_date);
+
+var baseURL = "https://www.eventbriteapi.com/v3/events/search/?sort_by=best&token=B7UELWTCRT7HCHZN3I7S";
+
+var queryURL = baseURL + "&start_date.range_start=" + start_date + "&start_date.range_end=" + end_date;
+
 //Need to get categories based off which picture is clicked, each picture should have onclick function that populates a categories into the query.
-// Example return Music & Conversation with Marty
-// Tue, Oct 23, 6:00 PM
-// The cutting room, newyork ny
-// starts at 27.00 on ticket fly (link to ticket)
-$.ajax({
-    url: queryURL,
-    method: "GET"
+
+
+// #portfolio is the section to add div to
+$("#gym").on("click", function () {
+
+    queryURL += "&categories=108"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (resp) {
+            // Example return Music & Conversation with Marty
+            // Tue, Oct 23, 6:00 PM
+            // The cutting room, newyork ny
+            // starts at 27.00 on ticket fly (link to ticket)
+            console.log(queryURL);
+            console.log(resp);
+            $("#portfolio").append("<div class=col-12>");
+        });
+
 })
-    .then(function () {
-});
