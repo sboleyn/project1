c7f62160ccf8123444dd78497c135dcc6120d25a
Samantha Boleyn <Samantha.Boleyn@maaxmicrosoft.com>
2018-09-26 16:30:32 -0700

    update master to see how input for country, city, and state is inputted

--- js/travelapi.js
+++ js/travelapi.js
@@ -42,7 +42,7 @@ $("#search-btn").on("click", function () {
     var end_date = moment($("#calendar-input").daterangepicker().val().split("-")[1].trim())["_i"];
     console.log(city);
     console.log(country);
-    var query = baseURL + "&start_date.range_start=" + start_date + "&start_date.range_end=" + end_date +"&location.address="+ city +"," + country;
+    var query = baseURL + "&start_date.range_start=" + start_date.format(dateFormat) + "&start_date.range_end=" + end_date.format(dateFormat) +"&location.address="+ city +"," + country;
     // + city +"," + country
     console.log(query);
 });
