adc344cda59d2f3ddee8673aa8847cc040df6dd1
Samantha Boleyn <Samantha.Boleyn@maaxmicrosoft.com>
2018-09-28 09:32:16 -0700

    fix creative.js

--- js/creative.js
+++ js/creative.js
@@ -8,8 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top )
-          // scrollTop: (target.offset().top - 56)
+          scrollTop: (target.offset().top - 56)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -24,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    // offset: 57
+    offset: 57
   });
 
   // Collapse Navbar
